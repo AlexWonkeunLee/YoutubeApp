a6eebca106130eb7c76e9991cd725ddad1f56d24
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2020-08-29 22:40:43 -0500

    added youtube api key

--- app/src/main/java/com/example/youtubeapp/alee/YoutubeActivity.java
+++ app/src/main/java/com/example/youtubeapp/alee/YoutubeActivity.java
@@ -1,15 +1,19 @@
 package com.example.youtubeapp.alee;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.ViewGroup;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    static final String GOOGLE_API_KEY = "AIzaSyDpzuVfOD4D-C0UjTyVbAnwoVZxVEeQiA4";
+    static final String YOUTUBE_VIDEO_ID = "ElpitAfkRS4";
+    static final String YOUTUBE_PLAYIST = "PLXtTjtWmQhg1SsviTmKkWO5n0a_-T0bnD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,14 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
         ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(layout);
 
-        Button button1 = new Button(this);
-        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
-        button1.setText("Button added");
-        layout.addView(button1);
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+//        button1.setText("Button added");
+//        layout.addView(button1);
+
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
     }
 
     @Override
